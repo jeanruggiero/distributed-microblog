c5bfd41655dead74904a68368d72ae91eda30fe3
Phillip Vo <phillip.t.vo@gmail.com>
2021-08-17 11:24:39 -0700

    Change server to only implement kv store interface on worker nodes

--- user_directory_service/src/__init__.py
+++ user_directory_service/src/__init__.py
@@ -1,5 +1,5 @@
 from typing import Dict
-from flask.app import Flask
+from flask import Flask
 
 d: Dict = {}
 d['data'] = {}
@@ -7,13 +7,13 @@ d['data'] = {}
 def create_app(coordinator=False) -> Flask:
     app = Flask(__name__)
 
-    from .store import store
-    app.register_blueprint(store, url_prefix='/store')
     if coordinator:
         from .coordinator import coordinator
         app.register_blueprint(coordinator, url_prefix='/coordinator')
     else:
+        from .store import store
         from .worker import worker
+        app.register_blueprint(store, url_prefix='/store')
         app.register_blueprint(worker, url_prefix='/worker')
 
     return app
