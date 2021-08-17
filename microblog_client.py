bc7c93648134b0c7203642362f50154f0d34a899
Jean Ruggiero <jeanruggiero@gmail.com>
2021-08-16 21:54:31 -0700

    Renamed client and added command line args for username and port

---
+++ microblog_client.py
@@ -0,0 +1,16 @@
+from microblog_app import AppInstance, MicroblogCommandLineInterface, User
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Run the microblogging command line client!')
+parser.add_argument('username', metavar='u', type=str, nargs=1, help='The username to connect with.')
+parser.add_argument('port', metavar='p', type=int, nargs=1, help='The port on which to run the application.')
+
+if __name__ == "__main__":
+
+    args = parser.parse_args()
+
+    username = args.username[0]
+    port = args.port[0]
+
+    MicroblogCommandLineInterface(AppInstance(User(username), port)).run()
